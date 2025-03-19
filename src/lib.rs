afe166d496b2c681627b837534553a8e4ccddc96
BurritoBandit28 <yansalmon@gmail.com>
2025-03-19 17:58:12 +0000

    Skip if folder is "venv". Option not to instantly save file, instead get file text

--- src/lib.rs
+++ src/lib.rs
@@ -3,29 +3,9 @@ use std::io::Write;
 use walkdir::WalkDir;
 
 /// Creates a md file of the project at the given directory, and then exports at the given output
-pub fn to_md(file_path: String, output_path : String, extension : String) -> String {
-    let mut return_string = String::new();
-    for dir in WalkDir::new(file_path.clone()) {
-        let dir = dir.unwrap();
-        let path = dir.path();
-        if path.to_str().unwrap().ends_with(&extension) {
+pub fn to_md_file(file_path: String, output_path : String, extension : String) -> String {
 
-            let split = path.to_str().unwrap().split(&file_path).collect::<Vec<&str>>();
-            let file_name = split.get(1).unwrap();
-
-            if file_name.starts_with("target") ||  file_name.starts_with("\\target") {continue}
-
-            println!("{}",file_name);
-
-            return_string = format!("{}\n# {}\n",return_string, file_name);
-
-            let code = read_to_string(path.clone()).unwrap();
-            let output = format!("```{}\n{}\n```\n", extension.strip_prefix(".").unwrap(),code);
-
-            return_string = format!("{}\n{}",return_string, output);
-
-        }
-    }
+    let mut return_string = to_md(file_path.clone(), extension.clone());
 
     let binding = file_path.replace("/", "\\");
     let split_path =  binding.split("\\").collect::<Vec<&str>>();
@@ -40,4 +20,31 @@ pub fn to_md(file_path: String, output_path : String, extension : String) -> Str
     let mut output_file = File::create(format!("{}\\{}.md",output_path.clone(), name));
     output_file.unwrap().write_all(return_string.as_bytes());
     format!("{}\\{}.md",output_path.clone(), name)
+}
+
+pub fn to_md(file_path: String, extension : String) -> String {
+    let mut return_string = String::new();
+    for dir in WalkDir::new(file_path.clone()) {
+        let dir = dir.unwrap();
+        let path = dir.path();
+        if path.to_str().unwrap().ends_with(&extension) {
+
+            let split = path.to_str().unwrap().split(&file_path).collect::<Vec<&str>>();
+            let file_name = split.get(1).unwrap();
+
+            if file_name.starts_with("target") ||  file_name.starts_with("\\target") || file_name.starts_with("venv") ||  file_name.starts_with("\\venv")  {continue}
+
+            println!("{}",file_name);
+
+            return_string = format!("{}\n# {}\n",return_string, file_name);
+
+            let code = read_to_string(path.clone()).unwrap();
+            let output = format!("```{}\n{}\n```\n", extension.strip_prefix(".").unwrap(),code);
+
+            return_string = format!("{}\n{}",return_string, output);
+
+        }
+    }
+    return_string
+
 }
